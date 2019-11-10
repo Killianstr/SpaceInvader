40cf0e70ec7077beb078b15e0ae837b730fb254a
Killianstr <stricot.killian@gmail.com>
2019-11-10 21:10:48 +0100

    ajout source

---
+++ SpaceInvaders/Vecteur2D.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceInvaders
+{
+    class Vecteur2D
+
+    {
+        private double x;
+        private double y;
+        public double norme;
+    }
+
+}
